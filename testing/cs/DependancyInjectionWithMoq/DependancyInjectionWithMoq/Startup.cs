b4cb414e6c930712e0eaf49b2a331576d043f0a6
Daniel Lewis <danlewis.ltd@gmail.com>
2015-02-19 09:21:29 +1300

    added project and enabled migrations

---
+++ testing/cs/DependancyInjectionWithMoq/DependancyInjectionWithMoq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DependancyInjectionWithMoq.Startup))]
+namespace DependancyInjectionWithMoq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
