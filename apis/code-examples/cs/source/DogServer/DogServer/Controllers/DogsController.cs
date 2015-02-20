f0068175054ca68a218b31d43cd76e0ddc4ce8a3
Daniel Lewis <danlewis.ltd@gmail.com>
2015-02-20 20:00:54 +1300

    added api controller and local controller to hit cat server

--- apis/code-examples/cs/source/DogServer/DogServer/Controllers/DogsController.cs
+++ apis/code-examples/cs/source/DogServer/DogServer/Controllers/DogsController.cs
@@ -8,7 +8,10 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using DogServer.Models;
+using WebGrease.Css.Extensions;
 
 namespace DogServer.Controllers
 {
@@ -20,6 +23,30 @@ namespace DogServer.Controllers
         public IQueryable<Dog> GetDog()
         {
             return db.Dog;
+        }
+
+        // POST: api/Dogs/Taint/5
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public HttpResponseMessage Taint([Bind(Include = "Id,Name,ImageUrl")] Dog dog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dog.Single(s => s == dog).ImageUrl = "http://bit.ly/17yp0G1";
+                db.SaveChanges();
+                return this.Request.CreateResponse(HttpStatusCode.Accepted);
+            }
+            return this.Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST: api/Dogs/resurrect
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public HttpResponseMessage Resurrect()
+        {
+            db.Database.Delete();
+            db.Database.Create();
+            return this.Request.CreateResponse(HttpStatusCode.Accepted);
         }
     }
 }
--- apis/code-examples/cs/source/DogServer/DogServer/Controllers/HomeController.cs
+++ apis/code-examples/cs/source/DogServer/DogServer/Controllers/HomeController.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using DogServer.Models;
+using RestSharp;
 
 namespace DogServer.Controllers
 {
     public class HomeController : Controller
     {
+        // Uses RestSharp for sending http requests to the cat server
         private DogDBContext db = new DogDBContext();
+        private RestClient catClient = new RestClient("http://example.com");
 
         // GET: Index
         public ActionResult Index()
         {
             return View(db.Dog.ToList());
+        }
+
+        // POST: TaintCat/5/
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TaintCat(int id)
+        {
+            var taint = new RestRequest(String.Format("/cats/{0}/taint", id), Method.PATCH);
+            catClient.Execute(taint);
+            return RedirectToAction("Index");
+        }
+
+        // POST: ResurectCats
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ResurectCats()
+        {
+            var res = new RestRequest("/cats/resurrect", Method.PATCH);
+            catClient.Execute(res);
+            return RedirectToAction("Index");
         }
     }
 }
