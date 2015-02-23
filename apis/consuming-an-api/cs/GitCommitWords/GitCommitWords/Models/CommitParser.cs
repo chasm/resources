0504c4e59195c09d49cc028b27be18178b1a5bc8
Eedrah <eedrah@users.noreply.github.com>
2015-02-23 22:42:54 +1300

    create commit parser model

---
+++ apis/consuming-an-api/cs/GitCommitWords/GitCommitWords/Models/CommitParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using RestSharp;
+
+namespace GitCommitWords.Models {
+    public class CommitParser {
+        private const string ApiDomain = "https://api.github.com";
+        private const string ApiResource = "users/{username}/events";
+
+        private static readonly RestClient GithubClient = new RestClient(ApiDomain);
+
+        public string Username { get; set; }
+        public Dictionary<string, int> WordCount { get; set; }
+
+        public CommitParser(string username) {
+            Username = username;
+            WordCount = new Dictionary<string, int>();
+        }
+
+        public void Parse() {
+            string json = MakeGithubRequest();
+            Process(json);
+        }
+
+        private string MakeGithubRequest() {
+            RestRequest request = new RestRequest(ApiResource, Method.GET);
+            request.AddUrlSegment("username", Username);
+            request.AddHeader("Accept", "application/vnd.github.v3+json");
+            IRestResponse response = GithubClient.Execute(request);
+            return response.Content;
+        }
+
+        private void Process(string json) {
+            throw new NotImplementedException();
+        }
+    }
+}
