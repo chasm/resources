4b06f17e761489312fb8b13111e29850b951793b
Daniel Lewis <danlewis.ltd@gmail.com>
2015-03-09 17:01:27 +1300

    updated solution

--- apis/code-examples/cs/source/DogServer/DogServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace DogServer.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- apis/code-examples/cs/source/DogServer/DogServer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DogServer.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
