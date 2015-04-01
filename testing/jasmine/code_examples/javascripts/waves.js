b9f28f24bc3b9b18c41c4526db5a31c428c46569
euglazer <eugenestapler@gmail.com>
2015-04-02 11:29:02 +1300

    updated code to include jQuery

--- testing/jasmine/code_examples/javascripts/waves.js
+++ testing/jasmine/code_examples/javascripts/waves.js
@@ -1,4 +1,35 @@
 function WaveColumn() {
+  this.divArray = [];
+}
+
+WaveColumn.prototype.initializeDivArray = function() {
+  for (var i = 0; i < 255; i++) {
+    this.divArray.push(this.generateRedDiv(i));
+  }
+}
+
+WaveColumn.prototype.generateRedDiv = function (opacity) {
+  var $div = $("<div class='line'></div>").css({
+    'background' : 'rgba(255,0,0,' + opacity / 255 + ')'
+  });
+  return $div;
+};
+
+WaveColumn.prototype.rotateDivArray = function() {
+  var div = this.divArray.shift();
+  this.divArray.push(div);
+}
+
+WaveColumn.prototype.print = function() {
+  var $column = $('#column');
+  $column.html('');
+  for (var i = 0; i < this.divArray.length; i++) {
+    $column.append(this.divArray[i]);
+  }
+}
+
+/* THE PURE JS WAY
+function WaveColumn() {
 	this.divArray = [];
 }
 
@@ -27,3 +58,4 @@ WaveColumn.prototype.print = function() {
 		column.appendChild(this.divArray[i]);
 	}
 }
+*/
--- testing/jasmine/code_examples/spec/waveSpec.js
+++ testing/jasmine/code_examples/spec/waveSpec.js
@@ -8,7 +8,7 @@ describe('WaveColumn()', function() {
 		it('is an Array', function() {
 			expect(this.waveColumn.divArray).toEqual(jasmine.any(Array));
 		});
-		it('is initially empty',function() {
+		it('is initially empty',function()	 {
 			expect(this.waveColumn.divArray.length).toEqual(0)
 		});
 	});
@@ -21,7 +21,100 @@ describe('WaveColumn()', function() {
 
 		it('populates divArray with 255 divs', function() {
 			expect(this.waveColumn.divArray.length).toEqual(255);
-		});	
+		});
+
+		it('each div has class \'line\'', function() {
+			expect(this.waveColumn.divArray[184].attr('class')).toEqual('line');
+		});
+
+		it('each div is red with opacity equal to 1 / 255 of its index in divArray', function() {
+			expect(this.waveColumn.divArray[100].css('background')).toEqual('rgba(255, 0, 0, 0.392157)');
+		});
+
+	});
+
+	describe('#generateRedDiv(opacity)', function() {
+
+		beforeAll(function() {
+			this.opacity = 42;
+			this.div = this.waveColumn.generateRedDiv(this.opacity);
+		});
+
+		it('returns a div', function() {
+			expect(this.div.prop('tagName')).toEqual('DIV');
+		});
+
+		it('the div has a class of \'line\'', function() {
+			expect(this.div.attr('class')).toEqual('line');
+		});
+
+		it('the div is the color red with opacity equal to 1 / 255 of the input parameter \'opacity\'', function() {
+			expect(this.div.css('background')).toEqual('rgba(255, 0, 0, 0.164706)')
+		});
+
+	});
+
+	describe('#rotateDivArray()', function() {
+		beforeAll(function() {
+			this.div = this.waveColumn.divArray[0]
+			this.waveColumn.rotateDivArray();
+		});
+
+		it('removes the first div in divArray', function() {
+			expect(this.waveColumn.divArray[0]).not.toEqual(this.div);
+		});
+
+		it('places the removed div at the back of divArray', function() {
+			expect(this.waveColumn.divArray[254]).toEqual(this.div);
+		});
+
+	});
+
+	describe('#print()', function() {
+		beforeAll(function() {
+			this.column = document.createElement('DIV')
+			this.column.id = 'column'
+			document.body.appendChild(this.column)
+			this.waveColumn.print();
+		});
+
+		it('appends every div in divArray to div#column in the html body', function() {
+			expect(this.column.childNodes.length).toEqual(255);
+		});
+
+		afterAll(function() {
+			document.body.removeChild(this.column)
+		});
+
+	});
+
+});
+
+/* THE PURE JS WAY
+describe('WaveColumn()', function() {
+
+	beforeAll(function() {
+		this.waveColumn = new WaveColumn();
+	});
+
+	describe('divArray', function() {
+		it('is an Array', function() {
+			expect(this.waveColumn.divArray).toEqual(jasmine.any(Array));
+		});
+		it('is initially empty',function()	 {
+			expect(this.waveColumn.divArray.length).toEqual(0)
+		});
+	});
+
+	describe('#initializeDivArray()', function() {
+
+		beforeAll(function() {
+			this.waveColumn.initializeDivArray();
+		});
+
+		it('populates divArray with 255 divs', function() {
+			expect(this.waveColumn.divArray.length).toEqual(255);
+		});
 
 		it('each div has class \'line\'', function() {
 			expect(this.waveColumn.divArray[184].className).toEqual('line');
@@ -75,11 +168,11 @@ describe('WaveColumn()', function() {
 			this.column = document.createElement('DIV')
 			this.column.id = 'column'
 			document.body.appendChild(this.column)
-			this.waveColumn.print();	
+			this.waveColumn.print();
 		});
 
 		it('appends every div in divArray to div#column in the html body', function() {
-			expect(this.column.childNodes.length).toEqual(255); 
+			expect(this.column.childNodes.length).toEqual(255);
 		});
 
 		afterAll(function() {
@@ -89,3 +182,4 @@ describe('WaveColumn()', function() {
 	});
 
 });
+*/
