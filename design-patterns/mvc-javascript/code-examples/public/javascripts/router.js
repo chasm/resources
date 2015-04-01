a3bfec3c3a5775b02c9f8ffa2804631db5813ce8
euglazer <eugenestapler@gmail.com>
2015-04-02 12:44:20 +1300

    add jQuery to example code

--- design-patterns/mvc-javascript/code-examples/public/javascripts/router.js
+++ design-patterns/mvc-javascript/code-examples/public/javascripts/router.js
@@ -1,18 +1,28 @@
-function bindListeners() {
-	document.body.onkeyup = function(e) {
-		var controller = new Controller();
-		if(e.keyCode == 13) {
-			controller.partyTime();
-		}
-	}	
-}
+$(document).ready(function () {
+  $('body').on('keyup', function(e) {
+    var controller = new Controller();
+    if (e.keyCode === 13) {
+      controller.partyTime();
+    }
+  });
+});
 
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-};
+// THE PURE JS WAY
+// function bindListeners() {
+// 	document.body.onkeyup = function(e) {
+// 		var controller = new Controller();
+// 		if(e.keyCode == 13) {
+// 			controller.partyTime();
+// 		}
+// 	}
+// }
 
-ready(bindListeners);
+// function ready(fn) {
+//   if (document.readyState != 'loading'){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// };
+
+// ready(bindListeners);
