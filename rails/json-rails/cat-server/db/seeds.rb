b8c461a80828fe337de24622a2b965a274371788
euglazer <eugenestapler@gmail.com>
2015-05-08 13:23:55 +1200

    clean slate rails

--- rails/json-rails/cat-server/db/seeds.rb
+++ rails/json-rails/cat-server/db/seeds.rb
@@ -1,3 +1,7 @@
-require 'faker'
-
-5.times { Cat.create(name: Faker::Name.name, bitcoin: rand(0.01..100.0))}
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
