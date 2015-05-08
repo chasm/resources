aaf5886fd3567618fb7223c2190ae190791880fb
euglazer <eugenestapler@gmail.com>
2015-05-08 13:26:22 +1200

    cat migration and seeds

--- rails/json-rails/cat-server/db/seeds.rb
+++ rails/json-rails/cat-server/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times { Cat.create(name: Faker::Name.name, bitcoin: rand(0.01..100.0))}
