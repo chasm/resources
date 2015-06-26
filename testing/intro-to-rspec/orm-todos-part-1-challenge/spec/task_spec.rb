eacbe68f4271c4c3b96ce17fb7c3ae9a0106ea3d
euglazer <eugenestapler@gmail.com>
2015-06-26 15:11:10 +1200

    formatting

--- testing/intro-to-rspec/orm-todos-part-1-challenge/spec/task_spec.rb
+++ testing/intro-to-rspec/orm-todos-part-1-challenge/spec/task_spec.rb
@@ -1,34 +1,25 @@
 require 'spec_helper'
 
 describe "Task" do
-
   let (:complete_task) { Task.create(description: "meow", completed: true) }
   let (:incomplete_task) { Task.create(description: "meow") }
 
   describe "#marker" do
-
     context "if complete" do
-
       it "returns 'X'" do
         expect(complete_task.marker).to eq("X")
       end      
-
     end
 
     context "if incomplete" do
-
       it "returns ' '" do
         expect(incomplete_task.marker).to eq(" ")
       end
-
     end
-
   end
 
   describe "#to_s" do
-
     context "if complete" do
-
       it "prints the task with a marker indicating it is complete" do
         expect(complete_task.to_s).to eq("[X] #{complete_task.id}. #{complete_task.description}")
       end
@@ -36,31 +27,24 @@ describe "Task" do
       it "prints the task without a marker indicating it is incomplete" do
         expect(incomplete_task.to_s).to eq("[ ] #{incomplete_task.id}. #{incomplete_task.description}")
       end
-
     end
-
   end
 
   describe "#tick!" do
-
     it "sets the task's completed attr to true" do
       incomplete_task.tick!
       expect(incomplete_task.completed).to be_truthy
     end
-
   end
 
   describe "#untick!" do
-
     it "sets the task's completed attr to false" do
       complete_task.untick!
       expect(complete_task.completed).to be_falsey
     end
-
   end
 
   after do
     Task.destroy_all
   end
-
 end
--- testing/intro-to-rspec/orm-todos-part-1-challenge/spec/tasks_controller_spec.rb
+++ testing/intro-to-rspec/orm-todos-part-1-challenge/spec/tasks_controller_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require_relative './../app/controllers/tasks_controller.rb'
 
 describe "TasksController" do
-
   let(:incomplete_task) { Task.create(description: Faker::Lorem.sentence) }
   let(:complete_task) { Task.create(description: Faker::Lorem.sentence, completed: true) }
 
@@ -11,32 +10,24 @@ describe "TasksController" do
   end
 
   describe "#print_list!" do
-
     context "if no tasks in the DB" do
-
       it "renders 'todo list is empty!" do
         expect(TasksController).to receive(:render!).with("todo list is empty!")
         TasksController.print_list!
       end
-
     end
 
     context "if tasks in the DB" do
-
       it "renders all tasks in the DB" do
         5.times { Task.create(description: Faker::Lorem.sentence) }
         expect(TasksController).to receive(:render!).with(Task.all)
         TasksController.print_list!
       end
-
     end
-
   end
 
   describe "#add_to_list!(description)" do
-
     context "if description is provided" do
-
       it "should add an item with given description to the DB" do
         TasksController.add_to_list!("meow")
         expect(Task.find_by_description("meow")).to be_truthy
@@ -46,15 +37,11 @@ describe "TasksController" do
         expect(TasksController).to receive(:render!).with("task successfully created")       
         TasksController.add_to_list!("meow")
       end
-
     end
-
   end
 
   describe "#delete_from_list!(id)" do
-
     context "if valid id" do
-
       it "deletes the task with given id from the DB" do
         TasksController.delete_from_list!(incomplete_task.id)
         expect(Task.find_by_id(incomplete_task.id)).to be_nil
@@ -64,24 +51,18 @@ describe "TasksController" do
         expect(TasksController).to receive(:render!).with("task successfully deleted")
         TasksController.delete_from_list!(incomplete_task.id)
       end
-
     end
 
     context "if invalid id" do
-
       it "should render an error message" do
         expect(TasksController).to receive(:render!).with("task does not exist")
         TasksController.delete_from_list!("not even an id")
       end
-
     end
-
   end
 
   describe "#tick!(id)" do
-
     context "if valid id" do
-
       it "should update task with given id's completed attr to true" do
         TasksController.tick!(incomplete_task.id)
         incomplete_task.reload
@@ -92,24 +73,18 @@ describe "TasksController" do
         expect(TasksController).to receive(:render!).with("task completed!")
         TasksController.tick!(incomplete_task.id)
       end
-
     end
 
     context "if invalid id" do
-
       it "should render an error message" do
         expect(TasksController).to receive(:render!).with("task does not exist")
         TasksController.tick!("not even an id")
       end
-
     end
-
   end
 
   describe "#untick!(id)" do
-
     context "if valid id" do 
-
       it "should update task with given id's completed attr to false" do
         TasksController.untick!(complete_task.id)
         complete_task.reload
@@ -120,22 +95,17 @@ describe "TasksController" do
         expect(TasksController).to receive(:render!).with("task set to incomplete")
         TasksController.untick!(complete_task.id)
       end
-
     end
 
     context "if invalid id" do
-
       it "should render an error message" do
         expect(TasksController).to receive(:render!).with("task does not exist")
         TasksController.untick!("not even an id")
       end
-
     end
-
   end
 
   after do
     Task.destroy_all
   end
-
 end
